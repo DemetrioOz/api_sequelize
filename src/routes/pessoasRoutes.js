941f591dee1fad5107cbeb2381c48fc8395b0cf1
MatheusD <55540506+MatheusDama@users.noreply.github.com>
2021-08-31 23:03:57 -0300

    termino do crud de Pessoas e criado outras models e migrations

--- src/routes/pessoasRoutes.js
+++ src/routes/pessoasRoutes.js
@@ -4,5 +4,9 @@ const router = Router();
 const PessoaController = require("../controllers/PessoaController");
 
 router.get("/pessoas", PessoaController.pegarTodasAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
 
 module.exports = router;
