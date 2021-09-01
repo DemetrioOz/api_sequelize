a5e8dac6cbfd533dd095e8535a2de8a36a1ecc38
MatheusD <55540506+MatheusDama@users.noreply.github.com>
2021-09-01 14:35:23 -0300

    criado alguns controllers e seeders

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const pessoas = require("./pessoasRoutes");
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
 
 module.exports = (app) => {
   app.use(express.json());
-  app.use(pessoas);
+
+  app.use(pessoas, niveis, turmas);
 
   app.get("/", (req, res) => {
     res.send("olá!");
